d33207ff518b47f3f6a4a140b9788e2388e35fd4
iH8Ra1n <ddwrtuuuu@gmail.com>
2018-05-23 13:00:04 -0400

    ADD JAILBREAK

---
+++ jailbreak.sh
@@ -0,0 +1,13 @@
+echo "Stigma is installing, please wait... "
+if [ ! -f already_run ]; then
+    apt-get update
+fi
+apt-get -y --force-yes install com.coolbooter.coolbootercli;
+echo;
+echo "Please enter final amount of desired storage in GB. (up to device capacity - 4)"
+read gb
+echo "WARNING! A LOT OF TEXT WILL SOON FLOOD THE SCREEN. EPILEPSY WARNING! "
+sleep 5
+coolbootercli 9.3.5 --datasize $gbGB
+apt-get -y --force-yes install com.coolbooter.cbuntether
+touch already_run
